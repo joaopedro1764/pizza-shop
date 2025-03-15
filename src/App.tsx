7991ef0e528a75aed85fac175a1838f389ec54b9
João Pedro Gomes da Silva <112516752+joaopedro1764@users.noreply.github.com>
2025-03-15 15:47:04 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 export function App() {
   return (
     <>
-      <RouterProvider router={router} />
-      <Toaster richColors closeButton />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="light">
+        <RouterProvider router={router} />
+        <Toaster richColors closeButton />
+      </ThemeProvider>
     </>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -4,12 +4,14 @@ import { SignIn } from "./pages/app/sign-in";
 import { AppLayout } from "./pages/_layout/app";
 import { AuthLayout } from "./pages/_layout/auth";
 import { SignUp } from "./pages/app/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
     children: [{ path: "/", element: <Dashboard /> }],
+    children: [{ path: "/orders", element: <Orders /> }],
   },
   {
     path: "/",
